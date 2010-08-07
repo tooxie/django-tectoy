1262777f769d46d41dac929ffc4a1c55c82b045b
Alvaro Mouriño <alvaro@mourino.net>
2010-08-07 19:58:58 -0300

    First commit

---
+++ apps/games/settings.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+ENGLISH = 'en'
+SPANISH = 'es'
+JAPANESE = 'jp'
+LANGUAGES = (
+    (ENGLISH, _(u'English')),
+    (SPANISH, _(u'Spanish')),
+    (JAPANESE, _(u'Japanese')),
+)
+TESTED_ON = (
+    ('5.00 M33-6', 'PSP 5.00 M33-6'),
+)
